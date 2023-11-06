f569a66698ef25dd4d12c51cb13f09a6586b3930
Yigit Yalcin <yigityalciin@hotmail.com>
2023-11-07 01:48:46 +0300

    Add project files.

---
+++ ne-izlesek-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ne_izlesek_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
